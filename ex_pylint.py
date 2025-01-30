009307b51d80e4def423a111467574fedc03b284
Kauã Morais <cmkaua@gmail.com>
2025-01-30 14:05:45 -0300

    Test of commit

---
+++ ex_pylint.py
@@ -0,0 +1,10 @@
+print("oi")
+
+
+def minha_funcao():
+    print('Estou na minha função')
+
+
+# snake_case -> my variable e funções em Python: my_variable V
+# cammelCase -> minhaFunção xxxxx
+# PascalCase -> Classe em Python
